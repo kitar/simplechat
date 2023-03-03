4e11710d12acb85be0a3c32f79d8a6ed3bb263cf
Satoshi Kita <inusurfer@gmail.com>
2023-03-04 01:03:46 +0900

    fix assets

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,6 +26,7 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
     wsHost: import.meta.env.VITE_PUSHER_HOST,
     wsPort: import.meta.env.VITE_PUSHER_PORT,
     wssPort: import.meta.env.VITE_PUSHER_PORT,
